75333fe0e9146822e020fdaa5c438db62c5f4602
Madhu Bhargav <madhubhargavp@gmail.com>
2019-02-15 20:27:36 -0600

    Update allowed hosts

--- portfolio_backend/portfolio_backend/settings.py
+++ portfolio_backend/portfolio_backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("ENV") == "DEVELOPMENT"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['madhubhargavp.pythonanywhere.com']
 
 
 # Application definition
